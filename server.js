975df7988401a426d93de5a25b18e8de3160cdac
Geoff Greer <geoff@greer.fm>
2012-04-24 14:09:16 -0700

    Start of email sending stuff

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require('express');
+var nodemailer = require("nodemailer");
 var settings = require('./settings');
 
 var app = express.createServer(express.logger());
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: settings.smtp_user,
+        pass: settings.smtp_pass
+    }
+});
 
 function check_commit(req, res) {
   var forced;
@@ -14,8 +24,29 @@ function check_commit(req, res) {
     payload = JSON.parse(req.body.payload);
     console.log(payload);
     if (payload.forced) {
-      console.log("OH SHIIIIIIIIIT");
-      
+      console.log("OH SHIIIIIIIIIT user", payload.pusher.name, "has forced a push");
+      console.log("compare at ", payload.compare);
+      var mailOptions = {
+          from: settings.smtp_user,
+          to: settings.recipients,
+          subject: "",
+          text: "User "
+      };
+
+      // send mail with defined transport object
+      smtpTransport.sendMail(mailOptions, function(error, response){
+          if(error){
+              console.log(error);
+          }else{
+              console.log("Message sent: " + response.message);
+          }
+
+          // if you don't want to use this transport object anymore, uncomment following line
+          //smtpTransport.close(); // shut down the connection pool, no more messages
+      });
+    }
+    else {
+      console.log("no forced push. user", payload.pusher.name, "is ok.");
     }
   }
 }
