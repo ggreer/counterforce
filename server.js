8d780ad4445742ad7e2efc1b6774fa9c25d386f8
Geoff Greer <geoff@greer.fm>
2012-04-24 15:33:12 -0700

    Code cleanup

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-var express = require('express');
+var express = require("express");
 var nodemailer = require("nodemailer");
-var settings = require('./settings');
-var util = require('util');
+var settings = require("./settings");
+var util = require("util");
 
 var app = express.createServer(express.logger());
 
-var smtp_transport = nodemailer.createTransport("SMTP",{
+var smtp_transport = nodemailer.createTransport("SMTP", {
   service: "Gmail",
   auth: {
     user: settings.smtp_user,
     pass: settings.smtp_pass
   }
 });
+
 
 function check_commit(req, res) {
   var forced;
@@ -21,35 +22,49 @@ function check_commit(req, res) {
     to: settings.recipients
   };
 
-  res.send('cool story, bro');
+  res.send("cool story, bro");
   console.log(req.body);
 
-  if (req.body && req.body.payload) {
-    payload = JSON.parse(req.body.payload);
-    console.log(payload);
-    if (payload.forced && settings.bad_refs.indexOf(payload.ref) !== -1) {
-      console.log("user", payload.pusher.name, "has forced a push to", payload.ref);
-
-      mail_opts.subject = util.format("%s: Push forced to master", payload.repository.name);
-      mail_opts.text = util.format("User %s forced a push to master in %s. Head is now at %s from %s", payload.pusher.name, payload.repository.name, payload.head_commit.id, payload.head_commit.timestamp);
-
-      smtp_transport.sendMail(mail_opts, function(error, response) {
-        if(error){
-          console.log(error);
-        }else{
-          console.log("Message sent:", response.message);
-        }
-      });
-    }
-    else {
-      console.log("no forced push. user", payload.pusher.name, "is ok.");
-    }
+  if (!req.body || !req.body.payload) {
+    console.log("No request.");
+    return;
   }
+
+  payload = JSON.parse(req.body.payload);
+  console.log(payload);
+
+  if (!payload) {
+    console.log("No payload or error parsing it.");
+    return;
+  }
+  if (!payload.forced) {
+    console.log("Push wasn't forced.");
+    return;
+  }
+  if (settings.bad_refs.indexOf(payload.ref) === -1) {
+    console.log("Push wasn't forced. User", payload.pusher.name, "is ok.");
+    return;
+  }
+
+  console.log("User", payload.pusher.name, "has forced a push to", payload.ref);
+
+  mail_opts.subject = util.format("%s: Push forced to master", payload.repository.name);
+  mail_opts.text = util.format("User %s forced a push to ref %s in repo %s. Head is now at %s from %s. Compare at %s", payload.pusher.name, payload.ref, payload.repository.name, payload.head_commit.url, payload.head_commit.timestamp, payload.compare);
+
+  smtp_transport.sendMail(mail_opts, function(error, response) {
+    if (error) {
+      console.log(error);
+    }
+    else{
+      console.log("Message sent:", response.message);
+    }
+  });
 }
 
-app.use(express.bodyParser());
-app.get('/', check_commit);
-app.post('/', check_commit);
 
-console.log('listening on port', settings.port);
+app.use(express.bodyParser());
+app.get("/", check_commit);
+app.post("/", check_commit);
+
+console.log("Listening on port", settings.port);
 app.listen(settings.port);
