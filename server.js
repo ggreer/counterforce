845b2fe7c3dbd0484bab324a37f675cf8638439e
Geoff Greer <geoff@greer.fm>
2012-04-24 00:18:45 -0700

    blah

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var settings = require('./settings');
 
-var app = express.createServer();
+var app = express.createServer(express.logger());
+
+function check_commit(req, res) {
+  res.send('cool story, bro');
+}
+
+app.get('/', check_commit);
+app.post('/', check_commit);
 
 console.log('listening on port', settings.port);
 app.listen(settings.port);
