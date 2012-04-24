700c75878721b5547edbbfe8c5afdcbe4e84992f
Geoff Greer <geoff@greer.fm>
2012-04-23 23:50:13 -0700

    Progress

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var settings = require('settings');
+
+var app = express.createServer();
+
+console.log('listening on port', settings.port);
+app.listen(settings.port);
